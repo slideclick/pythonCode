1199d3d9238c1d02e86c71014d1c533f6da75f76
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2015-07-01 12:25:49 +0800

    for ln in sys.stdin

--- regexp/pythonReg.py
+++ regexp/pythonReg.py
@@ -4,7 +4,7 @@ import re;
 import sys;
 datepat=re.compile(r'(\d+)[/-]?(\d+)[/-]?(\d+)\D([01][0-9]):([0-5][0-9])');
 [print(datepat.sub(r'\2/\3/\1T\4:\5',ln) if datepat.search(ln) else '' )for ln in open('log.txt') if ' ' in ln ]
-[print(datepat.sub(r'\2/\3/\1T\4:\5',ln) if datepat.search(ln) else '' )for ln in sys.stdin.readlines() if ' ' in ln ]
+[print(datepat.sub(r'\2/\3/\1T\4:\5',ln) if datepat.search(ln) else '' )for ln in sys.stdin if ' ' in ln ]
 #python pythonReg.py < log.txt
 [print(datepat.sub(lambda m: '%s/%s/%sT%s:%s' % (m.group(1),m.group(2),m.group(3),m.group(4),m.group(5)),ln) if datepat.search(ln) else '' )for ln in open('log.txt') if ' ' in ln ]
 [print(datepat.search(ln).group(0,1,2,3,4,5) if datepat.search(ln) else '' )for ln in open('log.txt') if ' ' in ln ]
