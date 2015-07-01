827490171567d9a6ac50d57b25153e09349804f3
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2015-07-01 12:24:11 +0800

    [print(datepat.sub(r'\2/\3/\1T\4:\5',ln) if datepat.search(ln) else '' )for ln in sys.stdin.readlines() if ' ' in ln ] #python pythonReg.py < log.txt

--- regexp/pythonReg.py
+++ regexp/pythonReg.py
@@ -1,8 +1,11 @@
 #python -c "
 from __future__ import print_function; 
 import re;
+import sys;
 datepat=re.compile(r'(\d+)[/-]?(\d+)[/-]?(\d+)\D([01][0-9]):([0-5][0-9])');
 [print(datepat.sub(r'\2/\3/\1T\4:\5',ln) if datepat.search(ln) else '' )for ln in open('log.txt') if ' ' in ln ]
+[print(datepat.sub(r'\2/\3/\1T\4:\5',ln) if datepat.search(ln) else '' )for ln in sys.stdin.readlines() if ' ' in ln ]
+#python pythonReg.py < log.txt
 [print(datepat.sub(lambda m: '%s/%s/%sT%s:%s' % (m.group(1),m.group(2),m.group(3),m.group(4),m.group(5)),ln) if datepat.search(ln) else '' )for ln in open('log.txt') if ' ' in ln ]
 [print(datepat.search(ln).group(0,1,2,3,4,5) if datepat.search(ln) else '' )for ln in open('log.txt') if ' ' in ln ]
 #"
