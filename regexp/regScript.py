3d524fbffb93ac25966c4d6f7af4835eaabf8a89
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2015-07-07 11:47:49 +0800

    add exAMPLE

--- regexp/regScript.py
+++ regexp/regScript.py
@@ -9,3 +9,5 @@ if datepat.search(ln) else None )\
 for ln in sys.stdin.readlines() \
 if ' ' in ln ]
 #python regScript.py < log.txt
+#2013-03-29 19:13:25:848 Info 2 960  
+#2013-04-01 15:58:53:432 Info 2 3864
