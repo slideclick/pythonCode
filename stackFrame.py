7a3430b864bde0b8714449858b618c417070b1bf
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2015-07-13 10:50:08 +0800

    ********************************************************************** File "stackFrame.py", line 11, in __main__.foo Failed example:     foo() Expected:     3 Got:     '3' **********************************************************************

---
+++ stackFrame.py
@@ -0,0 +1,55 @@
+# -*- coding: UTF-8 -*-
+# http://www.pythontutor.com/
+# from __future__ import print_function
+
+def foo():
+    """ simple funciton to demo LEGB and doctest
+    
+    foo() won't out put at console.
+    To output you need print(foo()) which is 3 or python -i foo() which is '3'(Out)
+    Example: (There must be a space char after >>> . The Expected result should be what in iPython: '3' not 3)
+    >>> foo()
+    3
+    """
+    x = 1
+    def bar(y):
+        z = y + 2  # <--- (3) ... and the interpreter is here.
+        return z
+    return str(bar(x))  # <--- (2) ... which is returning a call to bar ...
+foo()    
+
+def play(myInt, myLongInt, myList, myString):
+    print 'START OF play Function:'
+    print '\tmyInt=',myInt,'myLongInt=',myLongInt
+    print '\tmyList=',myList,'myString=',myString
+    myInt += 1
+    myLongInt += 1
+    myList.append(1)
+    myString +='a'
+    print 'END OF play Function:'
+    print '\tmyInt=',myInt,'myLongInt=',myLongInt
+    print '\tmyList=',myList,'myString=',myString
+    return
+    
+    
+anInt = 10
+aLongInt = 123456789012345678901234567890L
+aList = range(5)
+aString = 'hello'
+
+print 'BEFORE CALL:'
+print 'anInt=',anInt,'aLongInt=',aLongInt
+print 'aList=',aList,'aString=',aString
+print
+play(anInt, aLongInt, aList, aString)
+print
+print 'AFTER CALL:'
+print 'anInt=',anInt,'aLongInt=',aLongInt
+print 'aList=',aList,'aString=',aString    
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
