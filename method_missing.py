2eb50da31cf02fb367e851090376c62163936594
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2015-07-15 11:11:00 +0800

    __getattribute__()最容易解釋，一但定義有這個方法，任何特性的尋找都會被攔截，即使是那些__xxx__的內建特性名稱。
    __getattr__()的作用，是作為尋找特性的最後一個機會，类似ruby的method_missing

---
+++ method_missing.py
@@ -0,0 +1,38 @@
+# -*- coding: UTF-8 -*-
+# http://www.pythontutor.com/
+# python.exe -m doctest  stackFrame.py # stackFrame.py is argv to doctest.script
+# from __future__ import print_function
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __setattr__(self, name, value):
+        if not name in self.__dict__:
+            self.__dict__[name] = value
+        elif name == 'x' or name == 'y':
+            raise TypeError('Point(x, y) is immutable')
+    
+    def __eq__(self, that):
+        if not isinstance(that, Point):
+            return False
+        return self.x == that.x and self.y == that.y
+        
+    def __hash__(self):
+        return 41 * (41 + self.x) + self.y
+        
+    def __getattr__(self, name):
+      s = 'no def: ' + name + ' arity: %d'
+      return lambda *a: print(s % len(a))        
+
+p1 = Point(1, 1)
+p2 = Point(1, 1)
+p1 == p2
+pset = {p1}
+print(p1 in pset)
+#p1.x = 2 
+p1.z()
+
+
