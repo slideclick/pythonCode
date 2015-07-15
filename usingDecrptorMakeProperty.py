380658ce4ee8ccf34ed7f2916955e4ddb73f4dc1
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2015-07-15 15:41:40 +0800

    我给这个台湾的例子，加了deleter和setter.自己写描述符的陷阱是：你一定保证insta里面的field和class的不一样，否则del 里面del self.extension 会导致函数无限调用直到栈溢出。总体来还是老外那个python31里面那个更加专业

--- usingDecrptorMakeProperty.py
+++ usingDecrptorMakeProperty.py
@@ -5,14 +5,24 @@
 
 import inspect
 
-def prop(getter, setter, deleter):
+def prop(getter, setter=None,deleter=None):
     class PropDesc:
         def __get__(self, instance, owner):
             return getter(instance)
+            
         def __set__(self, instance, value):
-            setter(instance, value)
+            self.__setter(instance, value)
+            
         def __delete__(self, instance):
-            deleter(instance)
+            self.__deleter(instance)
+            
+        def deleter(self, deleter):
+            self.__deleter = deleter
+            return self        
+            
+        def setter(self, setter):
+            self.__setter = setter
+            return self   
             
     return PropDesc()
 class Ball:
@@ -34,3 +44,26 @@ class Ball:
         del self.__radius
         
     radius = prop(getRadius, setRadius, delRadius)    
+    
+class ABall:
+    def __init__(self, radius):
+        if radius <= 0:
+            raise ValueError('必須是正數')
+        self.__radius = radius
+    
+    @prop
+    def radius(self):
+        print('{0} calledBy {1}'.format(inspect.stack()[0][3],inspect.stack()[1][3]))
+        return self.__radius
+        
+    @radius.deleter    
+    def delRadius(self):
+        print('{0} calledBy {1}'.format(inspect.stack()[0][3],inspect.stack()[1][3]))
+        del self.__radius     
+        
+    @radius.setter    
+    def setRadius(self, radius):
+        print('{0} calledBy {1}'.format(inspect.stack()[0][3],inspect.stack()[1][3]))
+        self.__radius = radius
+        
+    
