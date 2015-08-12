80adab7bfca4733ee1c3cbcf114b415a8ed37527
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2015-08-12 11:08:07 +0800

    adding mysql and mssql example (you need install package first)

---
+++ exampleMySQL.py
@@ -0,0 +1,25 @@
+import os, sys  
+import MySQLdb  
+import pymssql
+import _mssql
+try:  
+    conn= MySQLdb.connect(host='localhost',user='root',passwd='',db='address'  )
+    #    conn = _mssql.connect(server='10.10.10.10', user='user', password='xxx',                             database='dbname')
+except Exception as e:  
+    print (e  )
+    sys.exit()  
+cursor=conn.cursor()  # sql server need not cursor
+sql='insert into address(name, address) values(%s, %s)'  
+value=(('zhangsan','haidian'),('lisi','haidian'))  # tuple's tuple, insert 2 lines
+try: 
+    cursor.executemany(sql,values)  #conn.execute_query
+except Exception as e:  
+    print (e  )
+sql='select * from address'  
+cursor.execute(sql)  # single statement
+data=cursor.fetchall()  
+if data  :
+    for x in data:  
+        print (x[0],x[1]  )
+cursor.close()  
+conn.close() 
